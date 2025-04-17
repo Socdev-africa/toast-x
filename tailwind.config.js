f8346465fcecc33b386071cfe9d29688e064f41d
salvatoluice <salvatoluice5@gmail.com>
2025-04-17 23:18:24 +0300

    -ft: initial commit and file directory set up

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'progress': 'progress var(--duration) linear forwards',
+      },
+      keyframes: {
+        progress: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
